04ac2f5c15086d3ed3d9cccc3be09e17497717a1
Samvrit Srinivas <sasrinivas@tesla.com>
2019-10-14 13:29:07 -0700

    change to UDP

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@
 #include "OdinWiFiInterface.h"
 #include "TCPSocket.h"
 #include "UDPSocket.h"
-#include <string> 
+#include <string>
 
 #ifdef DEVICE_WIFI_AP
 static const char *wifi_ssid = MBED_CONF_APP_WIFI_SSID;
@@ -28,10 +28,9 @@ static const char *ap_netmask = MBED_CONF_APP_AP_NETMASK;
 static const char *ap_gateway = MBED_CONF_APP_AP_GATEWAY;
 #endif
 
-#define ECHO_SERVER_PORT   5050
+#define ECHO_SERVER_PORT 5050
 
-OdinWiFiInterface   *_wifi;
-
+OdinWiFiInterface *_wifi;
 
 static void start_ap(nsapi_security_t security = NSAPI_SECURITY_WPA_WPA2)
 {
@@ -42,21 +41,20 @@ static void start_ap(nsapi_security_t security = NSAPI_SECURITY_WPA_WPA2)
     // AP Configure and start
     error_code = _wifi->set_ap_network(ap_ip, ap_netmask, ap_gateway);
     MBED_ASSERT(error_code == NSAPI_ERROR_OK);
-    
+
     //DHCP not available
     error_code = _wifi->set_ap_dhcp(false);
     MBED_ASSERT(error_code == NSAPI_ERROR_OK);
-    
+
     //Set beacon interval to default value
     _wifi->set_ap_beacon_interval(100);
-    
+
     //Set ap ssid, password and channel
     error_code = _wifi->ap_start(wifi_ssid, wifi_password, security, cbWLAN_CHANNEL_01);
     MBED_ASSERT(error_code == NSAPI_ERROR_OK);
-    
+
     printf("\nAP started successfully\n");
 }
-
 
 static void stop_ap()
 {
@@ -64,84 +62,81 @@ static void stop_ap()
 
     error_code = _wifi->ap_stop();
     MBED_ASSERT(error_code == NSAPI_ERROR_OK);
-    
-    printf("\nAP stopped\n");
 
+    printf("\nAP stopped\n");
 }
- 
+
 int main()
 {
     nsapi_size_or_error_t errcode;
     nsapi_error_t *err;
-    TCPSocket sock, *sock_data;
-    UDPSocket udpsock(_wifi);
+    // TCPSocket sock, *sock_data;
+    UDPSocket sock(_wifi);
+    SocketAddress sockAddr;
+    int in_data;
     int n = 0;
     char recv_buf[1024];
-    
+
     /*Start AP*/
-    
+
     _wifi = new OdinWiFiInterface(true);
 
-    start_ap(); 
-    
+    start_ap();
+
     /*Socket initialization*/
-    
+
     errcode = sock.open(_wifi);
-    if (errcode != NSAPI_ERROR_OK) {
+    if (errcode != NSAPI_ERROR_OK)
+    {
         printf("TCPSocket.open() fails, code: %d\n", errcode);
         return -1;
-    }  
+    }
 
     errcode = sock.bind("10.0.0.1", ECHO_SERVER_PORT);
-    if (errcode < 0) {
+    if (errcode < 0)
+    {
         printf("TCPSocket.connect() fails, code: %d\n", errcode);
         return -1;
     }
-    else {
+    else
+    {
         printf("TCP: connected with %s server\n", "10.0.0.1");
     }
-   
+
+    //Show the network address
+    const char *ip = _wifi->get_ip_address();
+    printf("IP address is: %s\n", ip ? ip : "No IP");
+
     /*Echo server*/
     if (sock.listen() == 0)
     {
-        sock_data = sock.accept(err = NULL);
-        
-        if (sock_data != NULL)
+        while (true)
         {
-            while (true)
-            {
-                n = sock_data->recv((void*) recv_buf, sizeof(recv_buf));
-                if (n > 0) 
-                {
-                    printf("\n Received from client %d bytes: %s \n", n, recv_buf);
-                    
-                    errcode = sock_data->send((void*) recv_buf, n);
-                    if (errcode < 0)
-                    {
-                        printf("\n TCPSocket.send() fails, code: %d\n", errcode);
-                        return -1;
-                    }
-                    else 
-                    {
-                        printf("\n TCP: Sent %d Bytes to client\n", n);
-                    }
-                }
-                else
-                {
-                    printf("\n TCPSocket.recv() failed");
-                    return -1;
-                }
-              
-            }
 
+            // char out_buffer[] = "time";
+            // if (0 > sock.sendto("time.nist.gov", 37, out_buffer, sizeof(out_buffer)))
+            // {
+            //     printf("Error sending data\n");
+            //     return -1;
+            // }
+
+            int n = sock.recvfrom(&sockAddr, &in_data, sizeof(recv_buf));
+            printf("Received %lu\n",
+                   (uint32_t)in_data);
+
+            printf("Address: %s Port: %d\n\r",
+                   sockAddr.get_ip_address(), sockAddr.get_port());
+
+            // Close the socket and bring down the network interface
+            sock.close();
+            _wifi->disconnect();
+            return 0;
         }
-
     }
-    
-    sock_data->close();
+
     sock.close();
-    
+
     stop_ap();
-                        
+
     return 0;
 }
