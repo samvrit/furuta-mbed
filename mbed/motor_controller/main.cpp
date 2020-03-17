74a92f6ae2cd8a69a2d18a2364e6b136cb2633af
Samvrit Srinivas <sasrinivas@tesla.com>
2020-03-17 07:53:46 +0000

    mbed: setup ADC and increase baud rate

--- mbed/motor_controller/main.cpp
+++ mbed/motor_controller/main.cpp
@@ -1,38 +1,59 @@
 #include "mbed.h"
 #include "rtos.h"
 
-PwmOut motor_pwm(PA_5);
+#define LPF_GAIN 0.3F
+#define CURRENT_SENSE_OFFSET 0.15F
+#define MAX_CURRENT_SENSE 7.6F
 
+PwmOut motor_pwm(PA_5);
 DigitalOut inA(PA_7);
 DigitalOut inB(PA_8);
-DigitalOut motor_enable(PB_8);
-
-AnalogIn currentSense(PA_6);
+AnalogIn currentSense(PA_0);
 
 InterruptIn button(BUTTON1);
 
-void flip() {
-    motor_enable = !motor_enable;
+Timer t;
+
+void flip(void)
+{
+    inA = !inA;
+    inB = 0;
+}
+
+void apply_lpf(float * output, float * input)
+{
+
+    *output += (*input - *output) * LPF_GAIN;
 }
 
 
 int main()
 {
     button.rise(&flip);
-    inA = 1;
+    inA = 0;
     inB = 0;
-    motor_enable = 0;
+
+    float duty_cycle = 0.9;
+    float currentSenseLPF = 0.0;
+    float currentSenseRaw = 0.0;
+
+    motor_pwm.period_us(500.0);
+    
     while(true)
     {
-        for(int i = 0; i <= 9; i++)
-        {
-            uint16_t senseVal = currentSense.read_u16();
-            float duty_cycle = (float)i*0.1;
-            motor_pwm.period_us(500.0);
-            motor_pwm.write(duty_cycle);
-            printf("Duty cycle: %.2f | Current: %u\r\n", duty_cycle, senseVal);
-            ThisThread::sleep_for(1000);
-        }
+        motor_pwm.write(duty_cycle);
+
+        currentSenseRaw = currentSense.read();
+
+        currentSenseRaw -= CURRENT_SENSE_OFFSET;
+
+        currentSenseRaw *= (MAX_CURRENT_SENSE / (1.0 - CURRENT_SENSE_OFFSET));\
+        
+        apply_lpf(&currentSenseLPF, &currentSenseRaw);
+
+        printf("%f\r\n", currentSenseLPF);
+
+        t.reset();
     }
         
     return 0; 
