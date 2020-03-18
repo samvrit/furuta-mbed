3a4869917c324bf084cc7a3ef462811152abeb57
Samvrit Srinivas <sasrinivas@tesla.com>
2020-03-18 07:03:30 +0000

    motor: write PI control loop

--- mbed/motor_controller/main.cpp
+++ mbed/motor_controller/main.cpp
@@ -4,6 +4,10 @@
 #define LPF_GAIN 0.3F
 #define CURRENT_SENSE_OFFSET 0.15F
 #define MAX_CURRENT_SENSE 7.6F
+#define MOTOR_CONSTANT_KT 0.2525F
+
+#define KP 0.257F
+#define KI 264.82F
 
 PwmOut motor_pwm(PA_5);
 DigitalOut inA(PA_7);
@@ -26,6 +30,11 @@ void apply_lpf(float * output, float * input)
     *output += (*input - *output) * LPF_GAIN;
 }
 
+float saturate(float input, float lower_limit, float upper_limit)
+{
+    return input > upper_limit ? upper_limit : ( input < lower_limit ? lower_limit : input);
+}
+
 
 int main()
 {
@@ -36,20 +45,27 @@ int main()
     float duty_cycle = 0.9;
     float currentSenseLPF = 0.0;
     float currentSenseRaw = 0.0;
+    float torqueFeedback = 0.0;
+    float torqueCommand = 0.1;
+    float torqueError = 0.0;
+    float torqueErrorIntegral = 0.0;
 
     motor_pwm.period_us(500.0);
     
     while(true)
     {
-        motor_pwm.write(duty_cycle);
-
         currentSenseRaw = currentSense.read();
-
         currentSenseRaw -= CURRENT_SENSE_OFFSET;
-
-        currentSenseRaw *= (MAX_CURRENT_SENSE / (1.0 - CURRENT_SENSE_OFFSET));\
-        
+        currentSenseRaw *= (MAX_CURRENT_SENSE / (1.0 - CURRENT_SENSE_OFFSET));
         apply_lpf(&currentSenseLPF, &currentSenseRaw);
+
+        torqueFeedback = currentSenseLPF * MOTOR_CONSTANT_KT;
+        torqueError = torqueCommand - torqueFeedback;
+        torqueErrorIntegral += torqueError;
+
+        duty_cycle = (KP * torqueError) + (KI * torqueErrorIntegral);
+        duty_cycle = saturate(duty_cycle, 0.01, 0.95);
+        motor_pwm.write(duty_cycle);
 
         printf("%f\r\n", currentSenseLPF);
 
