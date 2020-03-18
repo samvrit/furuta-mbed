09b97a8671b3f3f72818cf75e68bbe65fee675cf
Samvrit Srinivas <sasrinivas@tesla.com>
2020-03-18 09:47:04 +0000

    motor: implement torque control

--- mbed/motor_controller/main.cpp
+++ mbed/motor_controller/main.cpp
@@ -1,13 +1,18 @@
 #include "mbed.h"
 #include "rtos.h"
 
-#define LPF_GAIN 0.3F
+#define PI 3.14F
+
+#define CURRENT_LPF_CUTOFF_FREQ_HZ 200U
 #define CURRENT_SENSE_OFFSET 0.15F
 #define MAX_CURRENT_SENSE 7.6F
 #define MOTOR_CONSTANT_KT 0.2525F
 
-#define KP 0.257F
-#define KI 264.82F
+#define KP 6.57F
+#define KI 11503.0F
+
+#define DUTY_CYCLE_LOWER_BOUND 0.0F
+#define DUTY_CYCLE_UPPER_BOUND 1.0F
 
 PwmOut motor_pwm(PA_5);
 DigitalOut inA(PA_7);
@@ -18,16 +23,19 @@ InterruptIn button(BUTTON1);
 
 Timer t;
 
+bool motor_enable = false;
+
 void flip(void)
 {
     inA = !inA;
     inB = 0;
+    motor_enable = !motor_enable;
 }
 
-void apply_lpf(float * output, float * input)
+void apply_lpf(float * output, float * input, int dt, uint cutoff_freq)
 {
-
-    *output += (*input - *output) * LPF_GAIN;
+    float lpf_gain = (2.0 * PI * (float)cutoff_freq * (float)dt * 0.000001);
+    *output += (*input - *output) * lpf_gain;
 }
 
 float saturate(float input, float lower_limit, float upper_limit)
@@ -42,35 +50,43 @@ int main()
     inA = 0;
     inB = 0;
 
-    float duty_cycle = 0.9;
+    float duty_cycle = 0.0;
     float currentSenseLPF = 0.0;
     float currentSenseRaw = 0.0;
     float torqueFeedback = 0.0;
-    float torqueCommand = 0.1;
+    float torqueCommand = 0.02;
     float torqueError = 0.0;
     float torqueErrorIntegral = 0.0;
+    int dt = 0;
 
-    motor_pwm.period_us(500.0);
+    motor_pwm.period_us(100.0);
     
     while(true)
     {
-        currentSenseRaw = currentSense.read();
-        currentSenseRaw -= CURRENT_SENSE_OFFSET;
-        currentSenseRaw *= (MAX_CURRENT_SENSE / (1.0 - CURRENT_SENSE_OFFSET));
-        apply_lpf(&currentSenseLPF, &currentSenseRaw);
+        t.start();
 
-        torqueFeedback = currentSenseLPF * MOTOR_CONSTANT_KT;
-        torqueError = torqueCommand - torqueFeedback;
-        torqueErrorIntegral += torqueError;
+        if(motor_enable)
+        {
+            currentSenseRaw = currentSense.read();  // read from ADC
+            currentSenseRaw -= CURRENT_SENSE_OFFSET;    // adjust for offset
+            currentSenseRaw *= (MAX_CURRENT_SENSE / (1.0 - CURRENT_SENSE_OFFSET));  // scale the range
+            apply_lpf(&currentSenseLPF, &currentSenseRaw, dt, CURRENT_LPF_CUTOFF_FREQ_HZ); // apply low pass filter
 
-        duty_cycle = (KP * torqueError) + (KI * torqueErrorIntegral);
-        duty_cycle = saturate(duty_cycle, 0.01, 0.95);
-        motor_pwm.write(duty_cycle);
+            torqueFeedback = MOTOR_CONSTANT_KT * currentSenseLPF;   // calculate output torque (tau = Kt * i)
+            torqueError = torqueCommand - torqueFeedback;   // compute error signal
+            torqueErrorIntegral += torqueError; // compute integral of error signal
 
-        printf("%f\r\n", currentSenseLPF);
+            duty_cycle = (KP * torqueError) + (KI * torqueErrorIntegral);   // PI controller
+            duty_cycle = saturate(duty_cycle, DUTY_CYCLE_LOWER_BOUND, DUTY_CYCLE_UPPER_BOUND);  // saturate duty cycle
+            motor_pwm.write(duty_cycle);    // set duty cycle
+        }
+        
+        dt = t.read_us();
+
+        printf("%d,%f,%f,%f\r\n", dt, currentSenseLPF, torqueFeedback, duty_cycle);
 
         t.reset();
     }
-        
+
     return 0; 
 }
