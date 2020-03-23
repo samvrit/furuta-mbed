9f91ddd38df9e7371cc053a70f36f8b44ecf2cf7
Samvrit Srinivas <sasrinivas@tesla.com>
2020-03-23 07:16:19 +0000

    motor: add outer brackets for LPF macro

--- mbed/motor_controller/main.cpp
+++ mbed/motor_controller/main.cpp
@@ -15,7 +15,7 @@
 #define DUTY_CYCLE_UPPER_BOUND 0.99F
 
 #define SATURATE(input, lower_limit, upper_limit) ((input) > (upper_limit) ? (upper_limit) : ((input) < (lower_limit) ? (lower_limit) : (input)))
-#define LOW_PASS_FILTER(output, input, dt, cutoff_freq) ((output) += ((input) - (output)) * 2 * PI * (cutoff_freq) * (dt) * MICROSECOND)
+#define LOW_PASS_FILTER(output, input, dt, cutoff_freq) ((output) += (((input) - (output)) * 2 * PI * (cutoff_freq) * (dt) * MICROSECOND))
 
 DigitalOut motor_enable(PA_8);              // 3.3V power source for gate driver IC
 PwmOut motor_pwm(PA_5);                     // PWM for gate driver IC
