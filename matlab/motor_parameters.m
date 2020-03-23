55c36e0ea5d48edb4f5c306146313cb502f7f2ff
Samvrit Srinivas <sasrinivas@tesla.com>
2020-03-23 01:12:12 -0700

    make motor block an LTI block instead of a subsystem

--- matlab/motor_parameters.m
+++ matlab/motor_parameters.m
@@ -15,9 +15,12 @@ mass_data = [60; 50; 49; 84; 100; 74; 63; 61; 60; 60; 57]; % grams
 current_data = [1.2; 1.3; 1.24; 1.15; 1.12; 1.08; 1.03; 1.07; 1.06; 1.05; 1.05]; % Amperes
 arm_length = 0.447; %m
 
+% mass_data = [23; 62; 108]; % grams
+% current_data = [0.13; 0.4; 0.57]; % Amperes
+% arm_length = 0.447; %m
+
 % Determine K using linear regression
 K = current_data \ ((1e-3 * 9.81 * arm_length) .* mass_data);
-
 %% Experimental Transfer function
 % Experiment setup:
 % - Connect current sensor in series with motor and battery
@@ -28,25 +31,29 @@ K = current_data \ ((1e-3 * 9.81 * arm_length) .* mass_data);
 % function, which is a first order transfer function
 % - measure rising time, gain and delay if any
 
-rising_time = 535e-6; % seconds
-battery_step_amplitude = 12.66; % volts
-current_amplitude_at_steady_state = 7.2125; % amperes
+rising_time = 1.315e-3; % seconds
+battery_step_amplitude = 12.58; % volts
+current_amplitude_at_steady_state = 6.08; % amperes
 gain = current_amplitude_at_steady_state / battery_step_amplitude;
 
-motor_tf = tf(gain, [rising_time 1], 'InputDelay', 430e-6);
+motor_tf = tf(gain, [rising_time 1], 'InputDelay', 420e-6);
 disp(motor_tf)
 
-J = 9.9e-5;
+J = 0.0032; % moment of inertia of load (link 1)
 b = 1;
-R = 28.9;
-L = 0.05;
+R = 1/gain;
+L = R*rising_time;
 
-torque_ref = timeseries([0.01 0.02 -0.05 -0.03 0.04], [0.1 0.2 0.4 0.8 0.9]);
+torque_ref = timeseries([0.1 0.2 -0.5 -0.3 0.4], [0.1 0.2 0.4 0.8 0.9]);
 
 motor_sys = tf(1/R, [(b*L + K*J)/(R*b) 1]);
 open_loop = motor_sys*K;
 
-opts = pidtuneOptions('CrossoverFrequency',152,'PhaseMargin',90);
-[C, info] = pidtune(motor_tf, 'pid', opts);
+opts = pidtuneOptions('CrossoverFrequency',2.01e3,'PhaseMargin',76);
+[C, info] = pidtune(motor_sys*K, 'PI', opts);
+
+disp(C.Kp)
+disp(C.Ki)
+disp(C.Kd)
 
 closed_loop = (C*motor_sys) / (1 + (C*motor_sys*K));
