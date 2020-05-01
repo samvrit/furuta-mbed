bbeef4ad61dd90ee38a4bc07782d630abf82451d
Samvrit Srinivas <sasrinivas@tesla.com>
2020-05-01 07:51:24 +0000

    mbed: add code for encoder

--- mbed/system_controller/main.cpp
+++ mbed/system_controller/main.cpp
@@ -3,10 +3,14 @@
 #include "rtos.h"
 #include <string>
 
+#define nop 0x00            //no operation
+#define rd_pos 0x10         //read position
+#define set_zero_point 0x70 //set zero point
+#define timoutLimit 100
+
 Thread network_thread;
 
-DigitalOut can_ground(PA_10, 0);
-DigitalOut can_vref(PD_8, 1);
+SPI spi(D11, D12, D13, D10);
 CAN can1(PA_11, PA_12, 1000000);
 
 Timer timer;
@@ -20,8 +24,31 @@ typedef union {
 
 volatile canPacket_t canPacket;
 
+
+uint8_t SPIWrite(uint8_t sendByte)
+{
+  //holder for the received over SPI
+  uint8_t data;
+
+  //the AMT20 requires the release of the CS line after each byte
+  spi.select();
+  data = spi.write(sendByte);
+  spi.deselect();
+
+  //we will delay here to prevent the AMT20 from having to prioritize SPI over obtaining our position
+  
+  wait_us(50);
+  
+  return data;
+}
+
 int main()
 {
+    uint8_t data = 0;               //this will hold our returned data from the AMT20
+    uint8_t timeoutCounter = 0;     //our timeout incrementer
+    uint16_t currentPosition = 0;   //this 16 bit variable will hold our 12-bit position
+
+    spi.frequency(500000);
 
     canPacket.value = -0.5;
 
@@ -29,12 +56,41 @@ int main()
 
     ThisThread::sleep_for(2000);
 
+    // data = SPIWrite(set_zero_point);
+
+    // while (data != 0x80 && timeoutCounter++ < timoutLimit)
+    // {
+    //     data = SPIWrite(nop);
+    // }
+
     printf("Sending signal: %02X %02X %02X %02X\r\n", canPacket.buffer[0], canPacket.buffer[1], canPacket.buffer[2], canPacket.buffer[3]);
     can1.write(CANMessage(0x1, (const char *)&canPacket.buffer, sizeof(float)));
 
     while (1)
     {
         timer.start();
+
+        timeoutCounter = 0;
+    
+        data = SPIWrite(rd_pos);
+
+        while (data != rd_pos && timeoutCounter++ < timoutLimit)
+        {
+            data = SPIWrite(nop);
+        }
+
+        if (timeoutCounter < timoutLimit) //rd_pos echo received
+        {
+            currentPosition = (SPIWrite(nop)& 0x0F) << 8;
+        
+            currentPosition |= SPIWrite(nop);
+
+            printf("Current position: %u\r\n", currentPosition);
+        }
+
+        
+        ThisThread::sleep_for(100);
+
         osEvent evt = queue.get(0);
         if (evt.status == osEventMessage)
         {
