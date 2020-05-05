1fcf33af7920f55f51d2e22f3102bc479c37781b
Samvrit Srinivas <sasrinivas@tesla.com>
2020-05-05 07:26:49 +0000

    mbed: move encoder code to separate file

---
+++ mbed/system_controller/encoder_feedback.h
@@ -0,0 +1,6 @@
+#include "mbed.h"
+
+uint8_t SPIWrite(uint8_t sendByte);
+void encoder_init(void);
+void set_zero(void);
+float get_position(void);
--- mbed/system_controller/main.cpp
+++ mbed/system_controller/main.cpp
@@ -1,16 +1,12 @@
 #include "mbed.h"
 #include "acquire_feedback.h"
+#include "encoder_feedback.h"
 #include "rtos.h"
+#include "arm_math.h"
 #include <string>
-
-#define nop 0x00            //no operation
-#define rd_pos 0x10         //read position
-#define set_zero_point 0x70 //set zero point
-#define timoutLimit 100
 
 Thread network_thread;
 
-SPI spi(D11, D12, D13, D10);
 CAN can1(PA_11, PA_12, 1000000);
 
 Timer timer;
@@ -25,43 +21,16 @@ typedef union {
 volatile canPacket_t canPacket;
 
 
-uint8_t SPIWrite(uint8_t sendByte)
-{
-  //holder for the received over SPI
-  uint8_t data;
-
-  //the AMT20 requires the release of the CS line after each byte
-  spi.select();
-  data = spi.write(sendByte);
-  spi.deselect();
-
-  //we will delay here to prevent the AMT20 from having to prioritize SPI over obtaining our position
-  
-  wait_us(50);
-  
-  return data;
-}
-
 int main()
 {
-    uint8_t data = 0;               //this will hold our returned data from the AMT20
-    uint8_t timeoutCounter = 0;     //our timeout incrementer
-    uint16_t currentPosition = 0;   //this 16 bit variable will hold our 12-bit position
-
-    spi.frequency(500000);
-
+    
     canPacket.value = -0.5;
 
     network_thread.start(callback(sensors_receive));
 
     ThisThread::sleep_for(2000);
 
-    // data = SPIWrite(set_zero_point);
-
-    // while (data != 0x80 && timeoutCounter++ < timoutLimit)
-    // {
-    //     data = SPIWrite(nop);
-    // }
+    
 
     printf("Sending signal: %02X %02X %02X %02X\r\n", canPacket.buffer[0], canPacket.buffer[1], canPacket.buffer[2], canPacket.buffer[3]);
     can1.write(CANMessage(0x1, (const char *)&canPacket.buffer, sizeof(float)));
@@ -70,26 +39,9 @@ int main()
     {
         timer.start();
 
-        timeoutCounter = 0;
-    
-        data = SPIWrite(rd_pos);
-
-        while (data != rd_pos && timeoutCounter++ < timoutLimit)
-        {
-            data = SPIWrite(nop);
-        }
-
-        if (timeoutCounter < timoutLimit) //rd_pos echo received
-        {
-            currentPosition = (SPIWrite(nop)& 0x0F) << 8;
+        float position = get_position();
         
-            currentPosition |= SPIWrite(nop);
-
-            printf("Current position: %u\r\n", currentPosition);
-        }
-
-        
-        ThisThread::sleep_for(100);
+        ThisThread::sleep_for(5000);
 
         osEvent evt = queue.get(0);
         if (evt.status == osEventMessage)
