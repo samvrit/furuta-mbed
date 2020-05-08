43364386974a6bed25ba393d4b4c47e039126739
Samvrit Srinivas <sasrinivas@tesla.com>
2020-05-08 01:03:42 -0700

    arduino: use union for udp packets

--- arduino/Esp32UDPClient/Esp32UDPClient.ino
+++ arduino/Esp32UDPClient/Esp32UDPClient.ino
@@ -1,14 +1,18 @@
+//===================Includes===================//
 #include <WiFi.h>
 #include <WiFiUdp.h>
 #include <SPI.h>
 
+//===================Defines===================//
+#define PI 3.1415F
+#define ANGLE_SCALING_FACTOR (PI/(1U << 16U))
+
 //===================Global Variables===================//
-struct udp_frame
-{
-  unsigned short pos_rad;
-  uint8_t vel_sign;
-  unsigned int vel_rad;
-} frame;
+typedef union {
+    float value;
+    char buffer[sizeof(float)];
+} udpPacket_t;
+udpPacket_t udpPacket;
 unsigned long t0 = 0, t1 = 0, dt = 0;
 double velocity = 0.0;
 unsigned int previous_position;
@@ -57,41 +61,26 @@ void setup()
     ip = new IPAddress(10, 0, 0, 10);
 
   connectToWiFi(networkName, networkPswd);  //Connect to the WiFi network
-
-  frame.pos_rad = 0;
-  frame.vel_sign = 0;
-  frame.vel_rad = 0;
-}
-
-void serialize_frame(unsigned char* buffer, struct udp_frame* frame)
-{
-  memcpy(buffer, (const unsigned char*)&frame->pos_rad, 2);
-  memcpy(buffer + 2, (const unsigned char*)&frame->vel_sign, 1);
-  memcpy(buffer + 3, (const unsigned char*)&frame->vel_rad, 4);
 }
 
 void loop()
 {
-  frame.pos_rad = getAngle();
-  t1 = micros();
-  dt = t1 - t0;
-  velocity = 1000000 * (((1.0 * frame.pos_rad) - (1.0 * previous_position)) / dt);
-  previous_position = frame.pos_rad;
-  t0 = t1;
-  frame.vel_sign = velocity < 0 ? 1 : 0;
-  frame.vel_rad = velocity < 0 ? (unsigned int)(-1 * velocity * 100) : (unsigned int)(velocity * 100);
-  Serial.print(frame.pos_rad);
-  Serial.print(", ");
-  Serial.print(frame.vel_sign);
-  Serial.print(", ");
-  Serial.println(frame.vel_rad);
+  udpPacket.value = getAngle();
+  Serial.print("Position: ");
+  Serial.print(udpPacket.value);
+  Serial.print(" | ");
+  Serial.print(udpPacket.buffer[0], HEX);
+  Serial.print(" ");
+  Serial.print(udpPacket.buffer[1], HEX);
+  Serial.print(" ");
+  Serial.print(udpPacket.buffer[2], HEX);
+  Serial.print(" ");
+  Serial.println(udpPacket.buffer[3], HEX);
   if (wifi_connected)  //only send data when connected
   {
     //Send a packet
-    unsigned char buf7[7];
-    serialize_frame(buf7, &frame);
     udp.beginPacket(udpAddress, udpPort);
-    udp.write(buf7, 7);
+    udp.write((const uint8_t *)udpPacket.buffer, 4);
     udp.endPacket();
 
     if (udp.parsePacket())
--- arduino/Esp32UDPClient/MagneticSensor.ino
+++ arduino/Esp32UDPClient/MagneticSensor.ino
@@ -1,9 +1,11 @@
-unsigned int getAngle() {
+float getAngle() {
   uint16_t angle;
+  int16_t angle_signed;
   hspi->beginTransaction(SPISettings(spiClk, MSBFIRST, SPI_MODE0));
   digitalWrite(15, LOW);
   angle = hspi->transfer16(0x0000);
   digitalWrite(15, HIGH);
   hspi->endTransaction();
-  return (unsigned int)((angle*628)/65536.0);
+  angle_signed = angle > 32768 ? (int16_t)(angle - 32768) : (int16_t)angle;
+  return (float)(angle_signed * ANGLE_SCALING_FACTOR);
 }
