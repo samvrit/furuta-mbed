a667d72f14ed79619dd0b0e5b911c86f5a8451fe
Samvrit Srinivas <sasrinivas@tesla.com>
2020-05-08 04:14:32 +0000

    mbed: refine encoder code

--- mbed/system_controller/main.cpp
+++ mbed/system_controller/main.cpp
@@ -10,7 +10,7 @@ Thread network_thread;
 
 SPI encoder_spi(D11, D12, D13); // MOSI, MISO, CLK
 DigitalOut encoder_cs(D10); // chip select for encoder
-AMT203 encoder(&encoder_spi, &encoder_cs);
+AMT203 encoder(&encoder_spi, &encoder_cs, 1000000);
 
 CAN can1(PA_11, PA_12, 1000000);
 
@@ -46,16 +46,20 @@ int main()
 
         float position = encoder.get_position_minus_pi_to_plus_pi();
 
-        printf("Position: %.5f rad\r\n", position);
+        timer.stop();
+        dt = timer.read_us();
+        timer.reset();
+
+        printf("dt: %u, Position: %.5f rad\r\n", dt, position);
         
-        ThisThread::sleep_for(10);
+        //ThisThread::sleep_for(10);
 
         osEvent evt = queue.get(0);
         if (evt.status == osEventMessage)
         {
-            timer.stop();
-            dt = timer.read_us();
-            timer.reset();
+            // timer.stop();
+            // dt = timer.read_us();
+            // timer.reset();
             udp_frame *frame = (udp_frame *)evt.value.p;
             printf("\n%d | Received: %d, %d, %d\r\n", dt, frame->pos_rad, frame->vel_sign, frame->vel_rad);
 
