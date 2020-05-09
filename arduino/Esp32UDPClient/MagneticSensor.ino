57eb9ebe4eb6aff24760d119bb452af7d5e02c42
Samvrit Srinivas <sasrinivas@tesla.com>
2020-05-08 23:41:38 -0700

    arduino: make angle an int32

--- arduino/Esp32UDPClient/MagneticSensor.ino
+++ arduino/Esp32UDPClient/MagneticSensor.ino
@@ -1,11 +1,10 @@
 float getAngle() {
-  uint16_t angle;
-  int16_t angle_signed;
+  int32_t angle;
   hspi->beginTransaction(SPISettings(spiClk, MSBFIRST, SPI_MODE0));
   digitalWrite(15, LOW);
   angle = hspi->transfer16(0x0000);
   digitalWrite(15, HIGH);
   hspi->endTransaction();
-  angle_signed = angle > 32768 ? (int16_t)(angle - 32768) : (int16_t)angle;
-  return (float)(angle_signed * ANGLE_SCALING_FACTOR);
+  angle = angle > 32768 ? (angle - 32768) : angle;
+  return (float)(angle * ANGLE_SCALING_FACTOR);
 }
