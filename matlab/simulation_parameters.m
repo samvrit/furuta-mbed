c7e2e4eb2d358e45b38e2aecf6c244385c8dd03a
Samvrit Srinivas <sasrinivas@tesla.com>
2021-03-14 23:34:48 -0700

    Add discrete simulation
    Add transport delay
    
    Increase process variance
    
    tighten LQR gain for X4

--- matlab/simulation_parameters.m
+++ matlab/simulation_parameters.m
@@ -1,22 +1,28 @@
 load Matrices.mat
 
 x1_init_deg = 0;
-x2_init_deg = 2;
-x3_init_deg = 2;
+x2_init_deg = 0;
+x3_init_deg = 0;
 
 measurement_mean = 0;
-measurement_variance = 1e-6;
+measurement_variance = 1e-6; % (rad)
 
 actuator_noise_mean = 0;
-actuator_noise_variance = 0.0001;
+actuator_noise_variance = 0.05; % (N-m)
 
-Ts = 700e-6;
+process_noise_mean = 0;
+process_noise_variance = 1e-6; % (N-m)
 
-transport_delay_wifi = 5e-6;
+bearings_frictional_damping = 1e-9; % N-m/(rad/s)
+
+Ts = 200e-6; % (s)
+
+% transport_delay_wifi = 100e-6;
+transport_delay_wifi = 100e-6;
 zero_order_hold_measurement = 500e-6; % seconds -- this is the cycle time of high level controller
 zero_order_hold_torque_command = 100e-6;
 
-Q_process = 1;
+Q_process = 10;
 R_sensor = eye(3) .* measurement_variance;
 
 sys_ss = ss(A_matrix, B_matrix, C_matrix, D_matrix);
