ae2d4a8e0fe6e8d504abf1fdd2e4006d60aa93cd
Samvrit Srinivas <sasrinivas@tesla.com>
2022-12-03 00:58:34 -0800

    Make kalman filter config

--- matlab/kf_and_controller_wrapper.c
+++ matlab/kf_and_controller_wrapper.c
@@ -15,6 +15,7 @@
 /* %%%-SFUNWIZ_wrapper_includes_Changes_BEGIN --- EDIT HERE TO _END */
 #include <math.h>
 #include <stdbool.h>
+#include <string.h>
 #include "kalman_filter_lqr_lib/observer_controller.h"
 /* %%%-SFUNWIZ_wrapper_includes_Changes_END --- EDIT HERE TO _BEGIN */
 #define u_width 6
@@ -27,9 +28,11 @@
 /* %%%-SFUNWIZ_wrapper_externs_Changes_BEGIN --- EDIT HERE TO _END */
 /* extern double func(double a); */
 
-const float Ts = 0.0001f;
-float x_hat[6] = {0.0f};
+kf_input_S kf_input;
+kf_states_S kf_states;
+
 float torque_cmd = 0.0f;
+bool inverse_valid = true;
 /* %%%-SFUNWIZ_wrapper_externs_Changes_END --- EDIT HERE TO _BEGIN */
 
 /*
@@ -42,12 +45,56 @@ void kf_and_controller_Start_wrapper(real_T *xD)
 /*
  * Custom Start code goes here.
  */
+const float A[N_STATES][N_STATES] = {	{0.000000,	0.000000,	0.000000,	1.000000,	0.000000,	0.000000},
+										{0.000000,	0.000000,	0.000000,	0.000000,	1.000000,	0.000000},
+										{0.000000,	0.000000,	0.000000,	0.000000,	0.000000,	1.000000},
+										{0.000000,	82.411254,	-2.785977,	0.000000,	0.000000,	0.000000},
+										{0.000000,	130.127738,	-22.802044,	0.000000,	0.000000,	0.000000},
+										{0.000000,	-142.150128,	76.102558,	0.000000,	0.000000,	0.000000}};
 
-observer_init(x_hat, Ts);
+const float B[N_STATES][N_STATES] = {	{0.000000,	0.000000,	0.000000,	0.000000,	0.000000,	0.000000},
+										{0.000000,	0.000000,	0.000000,	0.000000,	0.000000,	0.000000},
+										{0.000000,	0.000000,	0.000000,	0.000000,	0.000000,	0.000000},
+										{147.219505,	0.000000,	0.000000,	0.000000,	0.000000,	0.000000},
+										{168.419459,	0.000000,	0.000000,	0.000000,	0.000000,	0.000000},
+										{-184.196878,	0.000000,	0.000000,	0.000000,	0.000000,	0.000000}};
+
+const float C[N_STATES][N_STATES] = {	{1.000000,	0.000000,	0.000000,	0.000000,	0.000000,	0.000000},
+										{0.000000,	1.000000,	0.000000,	0.000000,	0.000000,	0.000000},
+										{0.000000,	0.000000,	1.000000,	0.000000,	0.000000,	0.000000},
+										{0.000000,	0.000000,	0.000000,	0.000000,	0.000000,	0.000000},
+										{0.000000,	0.000000,	0.000000,	0.000000,	0.000000,	0.000000},
+										{0.000000,	0.000000,	0.000000,	0.000000,	0.000000,	0.000000}};
+
+const float K[N_STATES][N_STATES] = {	{3.1623,    -1.1589e3,    -3.2336e3,    35.0927,  -429.5221,  -464.7969},
+										{0.000000,	0.000000,	0.000000,	0.000000,	0.000000,	0.000000},
+										{0.000000,	0.000000,	0.000000,	0.000000,	0.000000,	0.000000},
+										{0.000000,	0.000000,	0.000000,	0.000000,	0.000000,	0.000000},
+										{0.000000,	0.000000,	0.000000,	0.000000,	0.000000,	0.000000},
+										{0.000000,	0.000000,	0.000000,	0.000000,	0.000000,	0.000000}};
+
+
+const float Q = 7.5e-5f;
+const float R = 1.21e-6f;
+
+memcpy(kf_input.A, A, N_STATES*N_STATES*sizeof(float));
+memcpy(kf_input.B, B, N_STATES*N_STATES*sizeof(float));
+memcpy(kf_input.C, C, N_STATES*N_STATES*sizeof(float));
+memcpy(kf_input.K, K, N_STATES*N_STATES*sizeof(float));
+
+kf_input.Q = Q;
+kf_input.R = R;
+kf_input.timestep = 5e-5f;
+
+observer_init(&kf_input, &kf_states);
 
 for(int i = 0; i < 20000; i++)
 {
-    covariance_matrix_step();
+    inverse_valid = covariance_matrix_step(&kf_input, &kf_states);
+    if (!inverse_valid)
+    {
+        break;
+    }
 }
 /* %%%-SFUNWIZ_wrapper_Start_Changes_END --- EDIT HERE TO _BEGIN */
 }
@@ -96,8 +143,9 @@ for (int i = 0; i < 6; i++)
 }
 
 // covariance_matrix_step();
-torque_cmd = control_output(x_hat, Ts);
-observer_step(measurement_lpf, true, x_hat);
+observer_step(measurement_lpf, inverse_valid, &kf_input, &kf_states);
+
+torque_cmd = control_output(&(kf_states.x_hat), kf_input.timestep, &kf_input);
 /* %%%-SFUNWIZ_wrapper_Update_Changes_END --- EDIT HERE TO _BEGIN */
 }
 
