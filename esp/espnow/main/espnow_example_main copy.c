0407c75e138ac361765abd341f2e997df5a3c206
samvrit <samvrit.goku@gmail.com>
2022-12-23 23:07:08 -0800

    Add transmit and receiver code

--- esp/espnow/main/espnow_example_main copy.c
+++ esp/espnow/main/espnow_example_main copy.c
@@ -28,22 +28,44 @@
 #include "esp_now.h"
 #include "esp_crc.h"
 #include "espnow_example.h"
+#include "driver/spi_master.h"
 #include "driver/gpio.h"
 #include "driver/timer.h"
 #include "esp_timer.h"
 
-#define ESPNOW_MAXDELAY 512
-
-#define GPIO_OUTPUT_IO_0    18
-#define GPIO_OUTPUT_PIN_SEL  (1ULL<<GPIO_OUTPUT_IO_0)
+#define GPIO_OUTPUT_IO_18    (18)
+#define GPIO_OUTPUT_PIN_SEL  (1ULL<<GPIO_OUTPUT_IO_18)
 
 #define TIMER_DIVIDER         16  //  Hardware timer clock divider
-#define TEST_WITHOUT_RELOAD   0        // testing will be done without auto reload
-#define TEST_WITH_RELOAD      1        // testing will be done with auto reload
+
+// #define RECEIVER_DEVICE (1U)
+// #define TRANSMIT_DEVICE1 (1U)
+#define TRANSMIT_DEVICE2 (1U)
+
+#define GPIO_MOSI 13
+#define GPIO_MISO 12
+#define GPIO_SCLK 14
+#define GPIO_CS 15
+
+#define PI (3.1415f)
+#define ANGLE_SCALING_FACTOR (2.0f * PI / 65535.0f)
+
+// Local types
+union uint32_to_float_T
+{
+    float value;
+    uint8_t raw[4];
+};
 
 static const char *TAG = "espnow_example";
 
-static uint8_t peer_mac[ESP_NOW_ETH_ALEN] = { 0x24, 0x0A, 0xC4, 0xEC, 0xA6, 0x28 };
+static uint8_t peer_mac[ESP_NOW_ETH_ALEN] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+
+#if (TRANSMIT_DEVICE1 || TRANSMIT_DEVICE2)
+union uint32_to_float_T angle_to_send = { .value = 0.0f };
+#else
+union uint32_to_float_T angle_received = { .value = 0.0f };
+#endif // (TRANSMIT_DEVICE1 || TRANSMIT_DEVICE2)
 
 /* WiFi should start before using ESPNOW */
 static void example_wifi_init(void)
@@ -67,49 +89,89 @@ static void example_espnow_send_cb(const uint8_t *mac_addr, esp_now_send_status_
 
 static void example_espnow_recv_cb(const uint8_t *mac_addr, const uint8_t *data, int len)
 {
-    gpio_set_level(GPIO_OUTPUT_IO_0, 1U);
+#if (TRANSMIT_DEVICE2)  // For transmit device 2, wait for 1ms after transmit device 1 has broadcast its position so as to not interfere
+
     timer_set_counter_value(TIMER_GROUP_0, TIMER_0, 0x00000000ULL);
     timer_start(TIMER_GROUP_0, TIMER_0);
 
+    // Wait for 1ms
+    // HW timer peripheral uses 80MHz clock as the base, and the divider is set to 16
+    // Hence, 1ms = 0.001 * (80000000/16) = 5000 counts
     uint64_t timer_val = 0UL;
-    while(timer_val < 10UL)
+    while(timer_val < 5000UL)
     {
         timer_get_counter_value(TIMER_GROUP_0, TIMER_0, &timer_val);
     }
     timer_pause(TIMER_GROUP_0, TIMER_0);
-    
-    gpio_set_level(GPIO_OUTPUT_IO_0, 0U);
+
+    esp_now_send(peer_mac, angle_to_send.raw, 4U);
+
+#elif (RECEIVER_DEVICE)
+
+    for(uint8_t i = 0; i < 4; i++)
+    {
+        angle_received.raw[i] = data[i];
+    }
+
+    ESP_LOGI(TAG, "Angle received from %02X %02X %02X %02X %02X %02X: %f\n", mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5], angle_received.value);
+
+#endif // RECEIVER_DEVICE
 }
 
+#if (TRANSMIT_DEVICE1)
 static void periodic_timer_callback(void* arg)
 {
-    gpio_set_level(GPIO_OUTPUT_IO_0, 1U);
-    const uint8_t data_to_send[4] = {0x12, 0x23, 0x34, 0x56};
-    esp_now_send(peer_mac, data_to_send, 4U);
-    gpio_set_level(GPIO_OUTPUT_IO_0, 0U);
+    esp_now_send(peer_mac, angle_to_send.raw, 4U);
 }
+#endif // (TRANSMIT_DEVICE1)
 
-static void example_espnow_task(void *pvParameter)
+#if (TRANSMIT_DEVICE1 || TRANSMIT_DEVICE2)
+static void mps_comms(void *pvParameter)
 {
-    vTaskDelay(1000 / portTICK_RATE_MS);
-    ESP_LOGI(TAG, "Start sending broadcast data");
+    spi_device_handle_t handle;
 
-    esp_timer_create_args_t periodic_timer_args = {0};
-    periodic_timer_args.callback = (esp_timer_cb_t)periodic_timer_callback;
-    periodic_timer_args.arg = NULL;
-    periodic_timer_args.dispatch_method = ESP_TIMER_TASK;
-    periodic_timer_args.name = "periodic_timer";
+    //Configuration for the SPI bus
+    spi_bus_config_t buscfg={
+        .mosi_io_num=GPIO_MOSI,
+        .miso_io_num=GPIO_MISO,
+        .sclk_io_num=GPIO_SCLK,
+        .quadwp_io_num=-1,
+        .quadhd_io_num=-1
+    };
 
-    esp_timer_handle_t periodic_timer;
+    //Configuration for the SPI device on the other side of the bus
+    spi_device_interface_config_t devcfg={
+        .clock_speed_hz=25000000,
+        .mode=0,
+        .spics_io_num=GPIO_CS,
+        .cs_ena_posttrans=3,        //Keep the CS low 3 cycles after transaction, to stop slave from missing the last bit when CS has less propagation delay than CLK
+        .queue_size=3
+    };
 
-    esp_timer_create(&periodic_timer_args, &periodic_timer);
+    spi_transaction_t t;
 
-    esp_timer_start_periodic(periodic_timer, 10000);
+    t.length = 16U;
+    t.flags = (SPI_TRANS_USE_TXDATA | SPI_TRANS_USE_RXDATA);
+
+    for(uint8_t i = 0U; i < 4U; i++)
+    {
+        t.tx_data[i] = 0x0U;
+    }
+
+    spi_bus_initialize(HSPI_HOST, &buscfg, 0);
+
+    spi_bus_add_device(HSPI_HOST, &devcfg, &handle);
 
     for(;;)
     {
+        spi_device_transmit(handle, &t);
+        uint16_t angle_little_endian = (t.rx_data[0] << 8U) | t.rx_data[1];
+        const float angle = angle_little_endian * ANGLE_SCALING_FACTOR;
+        vTaskDelay(1 / portTICK_RATE_MS);
+        angle_to_send.value = angle;
     }
 }
+#endif // (TRANSMIT_DEVICE1 || TRANSMIT_DEVICE2)
 
 static esp_err_t example_espnow_init(void)
 {
@@ -169,5 +231,21 @@ void app_main(void)
     example_wifi_init();
     example_espnow_init();
 
-    xTaskCreate(example_espnow_task, "example_espnow_task", 2048, NULL, 14, NULL);
+#if (TRANSMIT_DEVICE1)
+    esp_timer_create_args_t periodic_timer_args = {0};
+    periodic_timer_args.callback = (esp_timer_cb_t)periodic_timer_callback;
+    periodic_timer_args.arg = NULL;
+    periodic_timer_args.dispatch_method = ESP_TIMER_TASK;
+    periodic_timer_args.name = "periodic_timer";
+
+    esp_timer_handle_t periodic_timer;
+
+    esp_timer_create(&periodic_timer_args, &periodic_timer);
+
+    esp_timer_start_periodic(periodic_timer, 10000);
+#endif // (TRANSMIT_DEVICE1)
+
+#if (TRANSMIT_DEVICE1 || TRANSMIT_DEVICE2)
+    xTaskCreate(mps_comms, "mps_comms", 4096, NULL, 4, NULL);
+#endif // (TRANSMIT_DEVICE1 || TRANSMIT_DEVICE2)
 }
